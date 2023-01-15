4f23a99c6e67e4e53b26f31926f1cec2969dbfd0
voidhaze <voidhaze@gmail.com>
2023-01-15 17:55:00 +0000

    initial test descriptions

---
+++ pkg/jsoncfg_test.go
@@ -0,0 +1,35 @@
+package jsoncfg
+
+import (
+    "testing"
+)
+
+func TestLoadfiles(t *testing.T) {
+
+    // load dir
+    // load list
+    // load singular
+    // load invalid file
+
+    // chech master data merging is functioning correctly
+}
+
+func TestLoadfile(t *testing.T) {
+
+    // load valid
+    // load invalid
+    // load relative
+    // load absolute
+
+    // check master data has been updated correctly
+}
+
+func TestGet(t *testing.T) {
+    // get string
+    // get array
+    // get object
+    // get true
+    // get false
+    // get non existant / error
+}
+
