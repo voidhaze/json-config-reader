262a1bee6e5f4fae2d828fc54b271de0ae2841ab
voidhaze <voidhaze@gmail.com>
2023-01-16 17:39:12 +0000

    Update jsoncfg.go

--- jsoncfg.go
+++ jsoncfg.go
@@ -14,7 +14,8 @@ type Jsonconfig struct {
 	Masterdata interface{}
 }
 
-// Interface was created but not used, was intended to make the class able to handle alternative configs sources and data fetching methods
+// Interface was created but not used, was intended to make the class able to handle alternative configs sources and data fetching methods.
+// This needs abit more work to be functional, but roughly illustrates the intended function. 
 type Configforge interface {
 	Get(string)
 	Loadfile(string)
